3d492ededa5cb9e388dd95434066ecdaa636c035
Joseph Reilly <reilly4181@gmail.com>
2019-03-26 02:59:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ function happyHolidays() {
   return "Happy holidays!"
 }
 
-var name = "Stephanie"
+var name = "Stephanie";
+
 function happyHolidaysTo(name) {
   return  `Happy holidays, ${ name }!`
 }
